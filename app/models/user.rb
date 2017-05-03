392fd0c0665e38cf40d4347a60fe2580140da1a5
Kevin <buchard.kevin@gmail.com>
2017-05-03 07:45:21 +0000

    photo de profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :fullname, presence:true, length:{maximum: 65}       
+  
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/default_image.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
          
-         validates :fullname, presence:true, length:{maximum: 65}
+         
 end
