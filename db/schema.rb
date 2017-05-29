b6fc574930a5932cc2078265fc5442ea6c166fd4
Kevin <buchard.kevin@gmail.com>
2017-05-29 14:37:41 +0000

    modification formulaire demande de mot de passe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529132705) do
+ActiveRecord::Schema.define(version: 20170529142554) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
